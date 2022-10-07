ce73c37510f454d169e4d593b697e4661f716488
gleonag <gleonag@gmail.com>
2022-10-07 18:32:09 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,44 @@
+const { bottom } = require("@popperjs/core");
+
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+window.onload = () => {};
+{
+  document.querySelector(".number").innerHTML = generateRandomNumber();
+  let suitvalue = generateRandomSuit();
+  document.querySelector(".suit").innerHTML = suitvalue;
+  document.getElementById("bottom").innerHTML = suitvalue;
+}
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let generateRandomNumber(){
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers]
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let generateRandomSuit = () => {
+  let suit = ["♦", "♥" ,"♠", "♣"];
+  if (suit [indexSuit] === "♦" || suit[indexSuit] === "♥"){;
+    let color = document.getElementsByClassName("suit");
+    let colorArray = [...color];
+    colorArray.map(element => {
+      element.style.color = "red";
+      return element;
+    });
+    console.log(color);
+  }
+  return suit[indexSuit];
+}
